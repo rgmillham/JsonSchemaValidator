d6782ecf3769370372a30d98038ad867eee7743c
rickm <rgmillham@yahoo.com>
2023-06-14 11:23:19 -0600

    First commit of JSON validator

---
+++ src/main/java/com/espektrum/JsonSchemaValidator.java
@@ -0,0 +1,49 @@
+package com.espektrum;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.InputStream;
+
+/**
+ * Hello world!
+ */
+public class JsonSchemaValidator {
+
+    // Schema files should be in resources/schema
+    private static final String SCHEMA_FILE_NAME = "schema.json";
+
+    // Test files should be in resources/json
+    private static final String TEST_FILE_NAME = "test.json";
+
+    public static void main(String[] args) {
+        try (InputStream schemaInputStream = JsonSchemaValidator.class.getResourceAsStream(
+                String.format("/schema/%s", SCHEMA_FILE_NAME));
+             InputStream payloadInputStream = JsonSchemaValidator.class.getResourceAsStream(
+                     String.format("/json/%s", TEST_FILE_NAME))) {
+
+            assert schemaInputStream != null : "schemaInputStream is null";
+            assert payloadInputStream != null : "payloadInputStream is null";
+
+            // Get schema
+            final Schema schema = SchemaLoader.load(new JSONObject((new JSONTokener(schemaInputStream))));
+
+            // Get payload
+            final JSONObject rawPayload = new JSONObject(new JSONTokener(payloadInputStream));
+
+            // Validate
+            schema.validate(rawPayload);
+
+            System.out.println("Validation successful!");
+        } catch (ValidationException ex) {
+            // List validation errors
+            // ex.getAllMessages().forEach(System.out::println);
+            System.out.println(ex.toJSON().toString(4));
+        } catch (AssertionError | Exception ae) {
+            ae.printStackTrace();
+        }
+    }
+}
